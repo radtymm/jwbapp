a5edeab9ab0d05fcfc8222b7f86b919f96f39143
radtymm <308458093@qq.com>
2017-08-16 12:08:27 +0800

    jiemian

--- src/containers/ChatScreen.js
+++ src/containers/ChatScreen.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import {
     StyleSheet, ScrollView, navigator, Alert, View, Text, Button, FlatList, Dimensions, TouchableOpacity,
-    TouchableHighlight, Image, TextInput, Animated, Easing,
+    TouchableHighlight, Image, TextInput, Animated, Easing, RefreshControl,
 } from 'react-native';
 
 import styles from '../styleSheet/Styles';
 import {requestData, requestDataPost,} from '../libs/request.js';
 
 class ChatScreen extends React.Component {
-
-    static navigationOptions = {
-        headerTitle: "注册",
-        headerMode: "float",
-
-    };
 
     constructor(props, context) {
         super(props, context);
@@ -26,10 +20,49 @@ class ChatScreen extends React.Component {
 
     }
 
-    render() {
-        return (
-            <View style={{flex: 1, backgroundColor:"#0e6"}} >
+    handleRefresh() {
+      this.setState({isRefreshing: true})
+      // this.props.getContacts()
+      setTimeout(() => {
+        this.setState({isRefreshing: false})
+      }, 1000)
+    }
 
+    renderItem(item, index){
+
+        return (
+            <View>
+                <Text>{item.message}</Text>
+            </View>
+        );
+    }
+
+    render() {
+        let data = [
+            {headImage:require('../images/home.png'), message:"asdasdasd", isMe:true},
+            {headImage:require('../images/home.png'), message:"asdasdasd", isMe:true},
+            {headImage:require('../images/home.png'), message:"asdasdasd", isMe:true},
+        ];
+        return (
+            <View style={{flex: 1, backgroundColor:"#268"}} >
+                <FlatList
+                    data={data}
+                    keyExtractor = {(item, index) => ""+index}
+                    ref={"flat"}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={this.state.isRefreshing}
+                            onRefresh={()=>this.handleRefresh()}
+                            tintColor="#ff0000"
+                            title="加载中..."
+                            titleColor="#00ff00"
+                            colors={['#ff0000', '#00ff00', '#0000ff']}
+                            progressBackgroundColor="#ccc"
+                        />
+                    }
+                    renderItem={({item, index}) => this.renderItem(item, index)}
+
+                />
             </View>
         );
     }
